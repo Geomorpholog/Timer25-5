7f7fd9e0bfc010621dca9faeefa1723f3567da8d
Geomorpholog <148423934+Geomorpholog@users.noreply.github.com>
2024-05-09 20:48:50 +0300

    Update Button.js

--- src/Button.js
+++ src/Button.js
@@ -3,8 +3,8 @@ import UP from './UP.png'
 import Down from './Down.png'
 export default function Button(props){
     return <button className = "button" onClick = {props.method}> 
-        {props.dir == "up" && <img src={UP}></img>}
-        {props.dir == "down" && <img src={Down}></img>}
+        {props.dir == "up" && <img src={UP} alt = "Up arrow"></img>}
+        {props.dir == "down" && <img src={Down} alt = "Down arrow" ></img>}
     </button>
     
 }
