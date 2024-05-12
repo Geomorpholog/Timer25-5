04864c305e3ae4a6dd8eebb9e259a4f1e795f6ef
Geomorpholog <Geomorpholog@rambler.ru>
2024-05-12 19:49:56 +0300

    build F

--- src/Audio.js
+++
@@ -1,7 +0,0 @@
-
-export default function Audio(props) {
-    return (
-        <audio id={props.id} src='https://cdn.freecodecamp.org/testable-projects-fcc/audio/BeepSound.wav' preload ="auto"></audio>
-    )
-
-}
--- src/Control.js
+++
@@ -1,9 +0,0 @@
-import Button from './Button.js'
-export default function Control(props) {
-    return (
-        <div id={props.id}>
-            <Button method={props.methodPlay} dir={props.dirPlay} name ="start_stop" />
-            <Button method={props.methodReset} dir={props.dirReset} name="reset" />
-        </div>
-    )
-}
